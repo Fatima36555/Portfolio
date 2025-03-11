e72796b2c03cb0202b79baa1568805095d5d8476
Fatima36555 <alikhan3655555@gmail.com>
2025-03-11 14:52:10 +0500

    first commit

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
       className="text-white md:flex overflow-hidden items-center md:flex-wrap md:justify-center bg-purple-500 shadow-xl mx-0 md:mx-20 bg-opacity-40 rounded-lg p-12"
     >
       <div>
-        <h2 className="text-2xl md:text-4xl font-bold">About</h2>
+        <h2 className="text-2xl md:text-4xl font-bold text-center">About</h2>
         <div className="md:flex flex-wrap flex-col md:flex-row items-center">
           <img className="md:h-80" src={AboutImg} alt="About img" />
 
